1bf906ec4f486a93e95d34e56e18208845dcfdb3
xieyunjie <xieyunjie@users.noreply.github.com>
2019-08-29 09:30:30 +0800

    modify theme

--- WebAppCore.UI/config/config.js
+++ WebAppCore.UI/config/config.js
@@ -138,6 +138,7 @@ export default {
   // Theme for antd: https://ant.design/docs/react/customize-theme-cn
   theme: {
     'primary-color': primaryColor,
+    "font-size-base": "10px"
   },
   define: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
